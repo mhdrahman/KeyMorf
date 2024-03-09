60f665a097febae4ddf680555adb9b76277422d6
mhdrahman <mhd.rahman601@gmail.com>
2024-03-09 23:35:44 +0000

    Added layer class

---
+++ KeebSharp/Input/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace KeebSharp.Input
+{
+    public class Layer
+    {
+        public int Id { get; set; }
+
+        public Keys ToggleKey { get; set; }
+
+        public bool Active { get; set; };
+
+        public bool ToggleKeyDisabled { get; set; }
+
+        public Dictionary<Keys, Keys[]>? Mappings { get; set; }
+    }
+}
