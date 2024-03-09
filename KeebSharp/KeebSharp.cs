013d966b1ac00df99c713c127c665073dda52fcc
mhdrahman <mhd.rahman601@gmail.com>
2024-03-09 18:29:51 +0000

    Sending shifted keys

--- KeebSharp/KeebSharp.cs
+++ KeebSharp/KeebSharp.cs
@@ -1,6 +1,7 @@
 ﻿using KeebSharp.Interop;
 using KeebSharp.Logging;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -14,6 +15,12 @@ namespace KeebSharp
         private static IntPtr _hookId = IntPtr.Zero;
 
         private static IntPtr Handled = new(1);
+
+        // TODO: obviously this doesn't work, just making a single key to a macro but wanna test...
+        public static Dictionary<Keys, (Keys Key, bool Shift)[]> Mapping = new()
+        {
+            [Keys.J] = new[] { (Keys.N, true), (Keys.E, false), (Keys.W, false), (Keys.P, true), (Keys.A, false), (Keys.S, false), (Keys.S, false), (Keys.D1, false), (Keys.D2, false), (Keys.D3, false), (Keys.D1, true) },
+        };
 
         public static void Start()
         {
@@ -70,15 +77,56 @@ namespace KeebSharp
             if (nCode >= 0 && wParam == Hooks.WM_KEYDOWN)
             {
                 var vkCode = Marshal.ReadInt32(lParam);
-                var key = (Keys)vkCode;
+                var inputKey = (Keys)vkCode;
 
-                _logger.Info($"Key pressed {key}");
+                if (Mapping.TryGetValue(inputKey, out var mappedKeys))
+                {
+                    _logger.Info($"Mapped key pressed {inputKey}.");
+                    PressKeys(mappedKeys);
+                    return Handled;
+                }
 
-                // After we do our bit, let the key be processed as normal
+                // For any unmapped keys, let the key be processed as normal
                 return User32.CallNextHookEx(_hookId, nCode, Hooks.WM_KEYDOWN, (IntPtr)vkCode);
             }
 
             return User32.CallNextHookEx(_hookId, nCode, wParam, lParam);
         }
+
+        private static void PressKeys(params (Keys Key, bool Shift)[] keys)
+        {
+            foreach (var key in keys)
+            {
+                PressKey(key.Key, key.Shift);
+            }
+        }
+
+        private static void PressKey(Keys key, bool shift)
+        {
+            HoldShift(shift);
+            User32.keybd_event((byte)key, 0, 0, 0);
+            User32.keybd_event((byte)key, 0, 2, 0);
+            ReleaseShift(shift);
+        }
+
+        private static void HoldShift(bool shift)
+        {
+            if (!shift)
+            {
+                return;
+            }
+
+            User32.keybd_event((byte)Keys.RShiftKey, 0, 0, 0);
+        }
+
+        private static void ReleaseShift(bool shift)
+        {
+            if (!shift)
+            {
+                return;
+            }
+
+            User32.keybd_event((byte)Keys.RShiftKey, 0, 2, 0);
+        }
     }
-}
+} 
