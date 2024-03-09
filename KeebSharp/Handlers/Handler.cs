d752f31b790a1de71a895ead059055b7bae477f6
mhdrahman <mhd.rahman601@gmail.com>
2024-03-09 20:53:40 +0000

    Move all handling logic into a Handler class

---
+++ KeebSharp/Handlers/Handler.cs
@@ -0,0 +1,132 @@
+﻿using KeebSharp.Interop;
+using KeebSharp.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeebSharp.Handlers
+{
+    internal class Handler
+    {
+        private static Keys LayerToggleKey = Keys.A;
+        private static Stopwatch _toggleTimer = new Stopwatch();
+        private static bool _toggled = false;
+
+        private static Dictionary<Keys, (Keys Key, bool Shift)[]> Layer = new()
+        {
+            [Keys.Y] = new[] { (Keys.D9, true) },
+            [Keys.U] = new[] { (Keys.D0, true) },
+            [Keys.I] = new[] { (Keys.OemOpenBrackets, true) },
+            [Keys.O] = new[] { (Keys.OemCloseBrackets, true) },
+            [Keys.P] = new[] { (Keys.OemOpenBrackets, false) },
+            [Keys.OemSemicolon] = new[] { (Keys.OemCloseBrackets, false) },
+            [Keys.H] = new[] { (Keys.Left, false) },
+            [Keys.J] = new[] { (Keys.Down, false) },
+            [Keys.K] = new[] { (Keys.Up, false) },
+            [Keys.L] = new[] { (Keys.Right, false) },
+        };
+
+
+        private readonly ConsoleLogger _logger;
+        private readonly IntPtr _hookId;
+
+        public Handler(ConsoleLogger logger, IntPtr hookId)
+        {
+            _logger = logger;
+            _hookId = hookId;
+        }
+
+        public bool Handle(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == Hooks.WM_KEYDOWN)
+            {
+                var vkCode = Marshal.ReadInt32(lParam);
+                var inputKey = (Keys)vkCode;
+
+                if (inputKey == LayerToggleKey)
+                {
+                    if (!_toggleTimer.IsRunning)
+                    {
+                        _toggleTimer.Start();
+                        return false;
+                    }
+
+                    if (_toggleTimer.ElapsedMilliseconds < 500)
+                    {
+                        _toggled = !_toggled;
+                        _logger.Info(_toggled ? "Layer toggled." : "Layer disabled.");
+                        _toggleTimer.Stop();
+                        _toggleTimer.Reset();
+                        return true;
+                    }
+                    else
+                    {
+                        _toggleTimer.Stop();
+                        _toggleTimer.Reset();
+                    }
+                }
+
+                if (_toggled && Layer.TryGetValue(inputKey, out var mappedKeys))
+                {
+                    if (IsKeyPressed(Keys.LShiftKey))
+                    {
+                        _logger.Info("Left shift pressed");
+                    }
+
+                    _logger.Info($"Mapped key pressed {inputKey}.");
+                    PressKeys(mappedKeys);
+                    return true;
+                }
+
+                // For any unmapped keys, let the key be processed as normal
+                return false;
+            }
+
+
+            return false;
+        }
+
+        private static void PressKeys(params (Keys Key, bool Shift)[] keys)
+        {
+            foreach (var key in keys)
+            {
+                PressKey(key.Key, key.Shift);
+            }
+        }
+
+        private static void PressKey(Keys key, bool shift)
+        {
+            HoldShift(shift);
+            User32.keybd_event((byte)key, 0, 0, 0);
+            User32.keybd_event((byte)key, 0, 2, 0);
+            ReleaseShift(shift);
+        }
+
+        private static void HoldShift(bool shift)
+        {
+            if (!shift)
+            {
+                return;
+            }
+
+            User32.keybd_event((byte)Keys.RShiftKey, 0, 0, 0);
+        }
+
+        private static void ReleaseShift(bool shift)
+        {
+            if (!shift)
+            {
+                return;
+            }
+
+            User32.keybd_event((byte)Keys.RShiftKey, 0, 2, 0);
+        }
+
+        private static bool IsKeyPressed(Keys key)
+        {
+            return (User32.GetKeyState((int)Keys.LShiftKey) & 0x8000) > 0;
+        }
+    }
+}
--- KeebSharp/KeebSharp.cs
+++ KeebSharp/KeebSharp.cs
@@ -1,7 +1,7 @@
-﻿using KeebSharp.Interop;
+﻿using KeebSharp.Handlers;
+using KeebSharp.Interop;
 using KeebSharp.Logging;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -11,28 +11,11 @@ namespace KeebSharp
 {
     public static class KeebSharp
     {
-        private static ConsoleLogger _logger = new ConsoleLogger();
         private static IntPtr _hookId = IntPtr.Zero;
+        private static ConsoleLogger _logger = new ConsoleLogger();
+        private static Handler? _handler;
 
         private static IntPtr Handled = new(1);
-
-        private static Dictionary<Keys, (Keys Key, bool Shift)[]> Layer = new()
-        {
-            [Keys.Y] = new[] { (Keys.D9, true) },
-            [Keys.U] = new[] { (Keys.D0, true) },
-            [Keys.I] = new[] { (Keys.OemOpenBrackets, true) },
-            [Keys.O] = new[] { (Keys.OemCloseBrackets, true) },
-            [Keys.P] = new[] { (Keys.OemOpenBrackets, false) },
-            [Keys.OemSemicolon] = new[] { (Keys.OemCloseBrackets, false) },
-            [Keys.H] = new[] { (Keys.Left, false) },
-            [Keys.J] = new[] { (Keys.Down, false) },
-            [Keys.K] = new[] { (Keys.Up, false) },
-            [Keys.L] = new[] { (Keys.Right, false) },
-        };
-
-        private static Keys LayerToggleKey = Keys.A;
-        private static Stopwatch _toggleTimer = new Stopwatch();
-        private static bool _toggled = false;
 
         public static void Start()
         {
@@ -53,125 +36,49 @@ namespace KeebSharp
             };
 
             _logger.Info("KeebSharp is running. Press <C-c> to exit.");
+            _handler = new Handler(_logger, _hookId);
             Application.Run();
         }
 
         private static IntPtr SetKeyboardHook()
         {
-            using (var currentProcess = Process.GetCurrentProcess())
-            using (var currentModule = currentProcess.MainModule)
+            using (var process = Process.GetCurrentProcess())
+            using (var module = process.MainModule)
             {
-                if (currentModule == null)
+                if (module == null)
                 {
-                    _logger.Error($"ERROR: {nameof(currentModule)} was null.");
+                    _logger.Error($"ERROR: {nameof(module)} was null.");
                     return IntPtr.Zero;
                 }
 
-                if (currentModule.ModuleName == null)
+                if (module.ModuleName == null)
                 {
-                    _logger.Error($"ERROR: {nameof(currentModule.ModuleName)} was null.");
+                    _logger.Error($"ERROR: {nameof(module.ModuleName)} was null.");
                     return IntPtr.Zero;
                 }
 
-                var moduleHandle = Kernel32.GetModuleHandle(currentModule.ModuleName);
+                var moduleHandle = Kernel32.GetModuleHandle(module.ModuleName);
                 if (moduleHandle == IntPtr.Zero)
                 {
                     _logger.Error($"ERROR: {new Win32Exception(Marshal.GetLastWin32Error())}");
                     return IntPtr.Zero;
                 }
 
-                return User32.SetWindowsHookEx((int)Hooks.WH_KEYBOARD_LL, KeyboardHookCallback, Kernel32.GetModuleHandle(currentModule.ModuleName), 0);
+                return User32.SetWindowsHookEx((int)Hooks.WH_KEYBOARD_LL, KeyboardHookCallback, Kernel32.GetModuleHandle(module.ModuleName), 0);
             }
         }
 
         private static IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode >= 0 && wParam == Hooks.WM_KEYDOWN)
+            // _handler won't be null here because it's set in the Start function
+            var handled = _handler!.Handle(nCode, wParam, lParam);
+            if (handled)
             {
-                var vkCode = Marshal.ReadInt32(lParam);
-                var inputKey = (Keys)vkCode;
+                return Handled;
+            }    
 
-                if (inputKey == LayerToggleKey)
-                {
-                    if (!_toggleTimer.IsRunning)
-                    {
-                        _toggleTimer.Start();
-                        return User32.CallNextHookEx(_hookId, nCode, Hooks.WM_KEYDOWN, (IntPtr)vkCode);
-                    }
-
-                    if (_toggleTimer.ElapsedMilliseconds < 500)
-                    {
-                        _toggled = !_toggled;
-                        _logger.Info(_toggled ? "Layer toggled." : "Layer disabled.");
-                        _toggleTimer.Stop();
-                        _toggleTimer.Reset();
-                        return Handled;
-                    }
-                    else
-                    {
-                        _toggleTimer.Stop();
-                        _toggleTimer.Reset();
-                    }
-                }
-
-                if (_toggled && Layer.TryGetValue(inputKey, out var mappedKeys))
-                {
-                    if (IsKeyPressed(Keys.LShiftKey))
-                    {
-                        _logger.Info("Left shift pressed");
-                    }
-
-                    _logger.Info($"Mapped key pressed {inputKey}.");
-                    PressKeys(mappedKeys);
-                    return Handled;
-                }
-
-                // For any unmapped keys, let the key be processed as normal
-                return User32.CallNextHookEx(_hookId, nCode, Hooks.WM_KEYDOWN, (IntPtr)vkCode);
-            }
-
+            // For any unhandled keys, let the key be processed as normal
             return User32.CallNextHookEx(_hookId, nCode, wParam, lParam);
-        }
-
-        private static void PressKeys(params (Keys Key, bool Shift)[] keys)
-        {
-            foreach (var key in keys)
-            {
-                PressKey(key.Key, key.Shift);
-            }
-        }
-
-        private static void PressKey(Keys key, bool shift)
-        {
-            HoldShift(shift);
-            User32.keybd_event((byte)key, 0, 0, 0);
-            User32.keybd_event((byte)key, 0, 2, 0);
-            ReleaseShift(shift);
-        }
-
-        private static void HoldShift(bool shift)
-        {
-            if (!shift)
-            {
-                return;
-            }
-
-            User32.keybd_event((byte)Keys.RShiftKey, 0, 0, 0);
-        }
-
-        private static void ReleaseShift(bool shift)
-        {
-            if (!shift)
-            {
-                return;
-            }
-
-            User32.keybd_event((byte)Keys.RShiftKey, 0, 2, 0);
-        }
-
-        private static bool IsKeyPressed(Keys key)
-        {
-            return (User32.GetKeyState((int)Keys.LShiftKey) & 0x8000) > 0;
         }
     }
 } 
