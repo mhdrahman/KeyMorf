5a2fbd2a9948446d0f4ebd11d554a2248df53757
mhdrahman <mhd.rahman601@gmail.com>
2024-03-10 23:10:54 +0000

    Added logic to keyboard to determine programmatic keystrokes

--- KeebSharp/Input/Keyboard.cs
+++ KeebSharp/Input/Keyboard.cs
@@ -1,10 +1,14 @@
 ﻿using KeebSharp.Interop;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace KeebSharp.Input
 {
     internal static class Keyboard
     {
+        public const int Flag = 89911140;
+
         public static void Press(params (Keys Key, bool Shift)[] keys)
         {
             foreach (var key in keys)
@@ -16,9 +20,21 @@ namespace KeebSharp.Input
         public static void Press(Keys key, bool shift)
         {
             HoldShift(shift);
-            User32.keybd_event((byte)key, 0, 0, 0);
-            User32.keybd_event((byte)key, 0, 2, 0);
+            User32.keybd_event((byte)key, 0, 0, Flag);
+            User32.keybd_event((byte)key, 0, 2, Flag);
             ReleaseShift(shift);
+        }
+
+        public static bool IsProgrammatic(IntPtr lParam)
+        {
+            var hookStructObject = Marshal.PtrToStructure(lParam, typeof(User32.HookStruct));
+            if (hookStructObject == null)
+            {
+                return false;
+            }
+
+            var hookStruct = (User32.HookStruct)hookStructObject;
+            return (int)hookStruct.dwExtraInfo == Flag;
         }
 
         public static bool KeyHeld(Keys key)
