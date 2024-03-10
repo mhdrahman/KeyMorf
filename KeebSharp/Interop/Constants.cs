a88c4f647b42f609d013edd2b2e88541ce846c2c
mhdrahman <mhd.rahman601@gmail.com>
2024-03-10 13:50:34 +0000

    More warning fixes

--- KeebSharp/Interop/Constants.cs
+++ KeebSharp/Interop/Constants.cs
@@ -4,8 +4,8 @@ namespace KeebSharp.Interop
 {
     internal static class Constants
     {
-        public static IntPtr WH_KEYBOARD_LL = new (13);
-        public static IntPtr WM_KEYDOWN = new (0x0100);
-        public static IntPtr WM_KEYUP = new (0x0101);
+        public static readonly IntPtr WH_KEYBOARD_LL = new (13);
+        public static readonly IntPtr WM_KEYDOWN = new (0x0100);
+        public static readonly IntPtr WM_KEYUP = new (0x0101);
     }
 }
