4462a332836e704be502c4685a3e19433df27d56
mhdrahman <mhd.rahman601@gmail.com>
2024-03-10 15:36:30 +0000

    Explaination of event loop logic

--- KeebSharp/Program.cs
+++ KeebSharp/Program.cs
@@ -41,6 +41,10 @@ namespace KeebSharp
 
             _logger.Info("KeebSharp is running. Press <C-c> to exit.");
 
+            // This pumps a message loop. It allows Windows to break into the thread and make the call back.
+            // Passing IntPtr.Zero to GetMessage retrieves messages for any window that belongs to
+            // the current thread, and any messages on the current thread's message queue. As there
+            // are no windows, we should never actually recieve a message.
             var message = new User32.Message();
             while (User32.GetMessage(ref message, IntPtr.Zero, 0, 0))
             {
